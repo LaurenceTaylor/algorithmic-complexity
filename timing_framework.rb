179105fc88aa845c7d36f03b59166d0693080f49
Laurence Taylor <ltaylorapp@gmail.com>
2019-09-26 18:13:22 +0100

    Refactored TimingFramework

--- timing_framework.rb
+++ timing_framework.rb
@@ -1,23 +1,24 @@
 require './file_writer'
 
 class TimingFramework
-  MIN_ARRAY_SIZE = 5000
-  MAX_ARRAY_SIZE = 100_000
-  INTERVAL = 5000
+  MIN_INPUT_SIZE = 5000
+  MAX_INPUT_SIZE = 100_000
+  INPUT_SIZE_INTERVAL = 5000
   DEFAULT_METHODS = [:last, :reverse, :shuffle, :sort]
-  WARM_UP = 10
+  WARM_UP = 20
   REPEAT = 100
 
-  def initialize(methods = DEFAULT_METHODS, file_writer = FileWriter.new)
-    @methods = methods
+  def initialize(method_list: DEFAULT_METHODS, file_writer: FileWriter.new)
+    @method_list = method_list
     @file_writer = file_writer
   end
 
   def run
-    @current_array_size = MIN_ARRAY_SIZE
-    while @current_array_size <= MAX_ARRAY_SIZE
-      time_each_method(create_randomised_array(@current_array_size))
-      @current_array_size += INTERVAL
+    @current_input_size = MIN_INPUT_SIZE
+    while @current_input_size <= MAX_INPUT_SIZE
+      randomised_array = create_randomised_array(@current_input_size)
+      run_methods_on(randomised_array)
+      @current_input_size += INPUT_SIZE_INTERVAL
     end
   end
 
@@ -27,18 +28,19 @@ class TimingFramework
     [*1..size].shuffle
   end
 
-  def time_each_method(array)
-    @methods.each do |method|
-      entry = create_entry_string(time_method_repeater(method, array))
-      save_to_file(method, entry)
+  def run_methods_on(input)
+    @method_list.each do |method|
+      method_timings = produce_method_timings(method, input)
+      entry = create_entry_string(method_timings)
+      write_to_file(method, entry)
     end
   end
 
-  def time_method_repeater(method, array)
-    durations = []
-    WARM_UP.times { time_method { array.send(method) } }
-    REPEAT.times { durations << time_method { array.send(method) } }
-    durations
+  def produce_method_timings(method, input)
+    method_timings = []
+    WARM_UP.times { time_method { input.send(method) } }
+    REPEAT.times { method_timings << time_method { input.send(method) } }
+    method_timings
   end
 
   def time_method(&block)
@@ -48,11 +50,11 @@ class TimingFramework
     ((t1 - t0) * 1000.0).round(3)
   end
 
-  def create_entry_string(durations)
-    "#{@current_array_size}, #{durations.join(', ')}"
+  def create_entry_string(method_timings)
+    "#{@current_input_size}, #{method_timings.join(', ')}"
   end
 
-  def save_to_file(method, entry)
+  def write_to_file(method, entry)
     @file_writer.save(filename: method, entry: entry)
   end
 end
